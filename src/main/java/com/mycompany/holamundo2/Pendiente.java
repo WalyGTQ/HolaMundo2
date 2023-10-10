b592a3a3c0dc2ee6b80ee051cbeda688583d8463
walyn <walyn@HPCorei5>
2023-10-10 12:33:31 -0600

    creacion de la clases 2

---
+++ src/main/java/com/mycompany/holamundo2/Pendiente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.holamundo2;
+
+/**
+ *
+ * @author walyn
+ */
+public class Pendiente {
+    
+    private void texto(){
+        System.out.println("c");
+}
+    
+}
