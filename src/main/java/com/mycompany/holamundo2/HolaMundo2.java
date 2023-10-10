9a51ac4929d174fbebb8a755acf8a56858cfdb64
walyn <walyn@HPCorei5>
2023-10-10 11:46:50 -0600

    Un nuevo Hola

--- src/main/java/com/mycompany/holamundo2/HolaMundo2.java
+++ src/main/java/com/mycompany/holamundo2/HolaMundo2.java
@@ -15,6 +15,7 @@ public class HolaMundo2 {
         System.out.println("Hola2");
         System.out.println("Hola 3");
         System.out.println("dasd");  
+        System.out.println("Tercera PRueba");
         
     }
 }
